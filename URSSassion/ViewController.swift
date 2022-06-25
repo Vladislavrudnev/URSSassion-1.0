eec26060051ceac7fd1f2de25b46139b8d291415
emoge <vlad.rudnev.1992@mail.ru>
2022-06-25 11:20:57 +0600

    y

--- URSSassion/ViewController.swift
+++ URSSassion/ViewController.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func getimageButton(_ sender: Any) {
+        let API = "https://picsum.photos/200/300"
+        guard let apiURL = URL(string: API) else {
+            fatalError("Some Error")
+        }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: apiURL) { (data, respons, error ) in
+            guard let data = data, error == nil else {return}
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+            }
+
+        }
+        
+        task.resume()
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
